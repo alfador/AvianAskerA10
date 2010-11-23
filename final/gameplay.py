d92cc209773920a1d4925f68749b9a9914c306b7
Manuel Lagang <laganojunior@gmail.com>
2010-11-23 02:14:51 -0800

    Added a command-line argument to squelch useless outputs.
    The --hide command-line argument will switch the output of feedback
    on questions, and will only show the number of questions asked
    and the score at the end.

--- final/gameplay.py
+++ final/gameplay.py
@@ -1,6 +1,8 @@
 import random
 from BayesAsker_A10 import *## import *
 import cPickle
+import sys
+import os
 
 nspecies = 200
 nattributes = 288
@@ -38,6 +40,11 @@ dataset = open("./student/student_cPickle.txt","r")
 data = cPickle.load(dataset)
 dataset.close()
 
+# Set output file
+out = sys.stdout
+if '--hide' in sys.argv:
+    out = file(os.devnull, 'w')
+
 #Begin new game
 Sum = 0
 n = 100
@@ -52,41 +59,44 @@ for i in range(n):
         while True:
                 Q = myAvianAsker(image[image_id], QAs)
                 if Q-nattributes+1 == rndbrd:
-                        print("Is it "+spec_dict[Q-nattributes+1]+"?")
-                        print("You have guessed correctly, the bird is "+spec_dict[rndbrd]+"\n")
-                        print("Your score is "+str(len(QAs)+1))
+                        print >> out, ("Is it "+spec_dict[Q-nattributes+1]+"?")
+                        print >> out, ("You have guessed correctly, the bird is "+spec_dict[rndbrd]+"\n")
+                        print >> out, ("Your score is "+str(len(QAs)+1))
                         Sum = Sum + len(QAs) + 1
                         break
                 elif Q >= nattributes + nspecies:
-                        print("The question is out of range")
+                        print >> out, ("The question is out of range")
                         continue
                 elif Q >= nattributes and Q != rndbrd:
-                        print("Is it "+spec_dict[Q-nattributes+1]+"?")
-                        print("Sorry, you are wrong!\n")
+                        print >> out, ("Is it "+spec_dict[Q-nattributes+1]+"?")
+                        print >> out, ("Sorry, you are wrong!\n")
                         A = '0' #incorrect guess
                 else:                
-                        print("It "+ Ques_dict[str(Q)] +"?")
+                        print >> out, ("It "+ Ques_dict[str(Q)] +"?")
                         if Q in data[image_id].keys():
                                 A = random.choice(data[image_id][Q])
                         else:
                                 A = '2'
                         print(str(Q)+" "+str(A))
                         if A == [1,0]:
-                                print("Yes! Probably.\n")
+                                print >> out,("Yes! Probably.\n")
                         elif A == [1,1]:
-                                print("Yes! Definitely.\n")
+                                print >> out,("Yes! Definitely.\n")
                         elif A == [1,2]:
-                                print("Yes! I guess.\n")
+                                print >> out,("Yes! I guess.\n")
                         elif A == [0,0]:
-                                print("No! Probably.\n")
+                                print >> out,("No! Probably.\n")
                         elif A == [0,1]:
-                                print("No! Definitely.\n")
+                                print >> out,("No! Definitely.\n")
                         elif A == [0,2]:
-                                print("No! I guess.\n")
+                                print >> out,("No! I guess.\n")
                         else:
-                                print("I don't know!\n")
+                                print >> out,("I don't know!\n")
 
                 QAs.append([Q, A])
-        print("Num is " + str(i+1) + ", Sum is "+str(Sum)+", Score now is "+str(Sum/(i+1)))
-print("Your final score is "+str(Sum/n))
 
+                f = open("QA.txt", "w")
+                f.write(str(QAs))
+                f.close()
+        print "Num is " + str(i+1) + ", Sum is "+str(Sum)+", Score now is "+str(Sum/(i+1))
+print "Your final score is "+str(Sum/n)
